64e61612fbb301b487334737edc590dc1e05152f
Esmanur <esmakdgl61@gmail.com>
2026-09-23 17:28:26 +0300

    Add simple calculator project

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = input.nextInt();
+
+        int sum = number1 + number2;
+        System.out.println("Sum: " + sum);
+        int difference = number1 - number2;
+        System.out.println("Difference: " + difference);
+        int product = number1 * number2;
+        System.out.println("Product: " + product);
+
+    }
+}
