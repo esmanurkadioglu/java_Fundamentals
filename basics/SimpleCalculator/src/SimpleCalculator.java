4e1c5c54bbcefa0d02b53afccec12b5e561d4a4f
Esmanur <esmakdgl61@gmail.com>
2026-09-26 23:20:28 +0300

    Update SimpleCalculator

--- basics/SimpleCalculator/src/SimpleCalculator.java
+++ basics/SimpleCalculator/src/SimpleCalculator.java
@@ -5,17 +5,38 @@ public class SimpleCalculator {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
-        int number1 = input.nextInt();
+        double number1 = input.nextDouble();
 
         System.out.print("Enter second number: ");
-        int number2 = input.nextInt();
+        double number2 = input.nextDouble();
 
-        int sum = number1 + number2;
-        System.out.println("Sum: " + sum);
-        int difference = number1 - number2;
-        System.out.println("Difference: " + difference);
-        int product = number1 * number2;
-        System.out.println("Product: " + product);
+        System.out.println("\nChoose an operation:");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraction");
+        System.out.println("3 - Multiplication");
+        System.out.println("4 - Division");
 
+        System.out.print("Enter your choice: ");
+        int choice = input.nextInt();
+
+        if(choice == 1) {
+            double result = number1 + number2;
+            System.out.println("Result: " + result);
+        } else if (choice ==2) {
+            double result = number1 - number2;
+            System.out.println("Result: " + result);
+        } else if (choice == 3) {
+            double result = number1 * number2;
+            System.out.println("Result: " + result);
+        } else if (choice == 4) {
+            if(number2 == 0) {
+                System.out.println("Error: Cannot divide by zero.");
+            } else {
+                double result = number1 / number2;
+                System.out.println("Result: " + result);
+            }
+        } else {
+            System.out.println("Invalid choice.");
+        }
     }
 }
